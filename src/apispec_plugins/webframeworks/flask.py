fd2ed206f567eafe9b096817e4a68a7245297beb
Renato Teixeira <rena2damas@gmail.com>
2022-01-30 16:04:16 +0000

    refactor: redo path_helper #wip

--- src/apispec_plugins/webframeworks/flask.py
+++ src/apispec_plugins/webframeworks/flask.py
@@ -8,33 +8,43 @@ from apispec.exceptions import APISpecError
 class FlaskPlugin(BasePlugin):
     """APISpec plugin for Flask"""
 
+    def init_spec(self, spec):
+        super().init_spec(spec)
+
+        # resource discovery
+        for _, view in current_app.view_functions.items():
+            self.path_helper(view=view, app=current_app)
+
     @staticmethod
-    def _rule_for_view(view, app=None):
+    def _rule_view(view, app=None):
         if app is None:
             app = current_app
 
-        view_funcs = app.view_functions
         endpoint = None
-        for ept, view_func in view_funcs.items():
+        for ept, view_func in app.view_functions.items():
             if view_func == view:
                 endpoint = ept
         if not endpoint:
             raise APISpecError(f"Could not find endpoint for view {view}")
 
         # WARNING: Assume 1 rule per view function for now
-        rule = app.url_map._rules_by_endpoint[endpoint][0]
+        rule = app.url_map.iter_rules(endpoint=endpoint)[0]
         return rule
 
-    def path_helper(self, operations, *, view, app=None, **kwargs):
+    def path_helper(
+            self,
+            view=None,
+            app=None,
+            **kwargs
+    ):
         """Path helper that allows passing a Flask view function."""
-        rule = self._rule_for_view(view, app=app)
-        operations.update(yaml_utils.load_operations_from_docstring(view.__doc__))
-        if hasattr(view, "view_class") and issubclass(view.view_class, MethodView):
-            for method in view.methods:
-                if method in rule.methods:
-                    method_name = method.lower()
-                    method = getattr(view.view_class, method_name)
-                    operations[method_name] = yaml_utils.load_yaml_from_docstring(
-                        method.__doc__
-                    )
+        rule = self._rule_view(view, app=app)
+        # if hasattr(view, 'view_class') and issubclass(view.view_class, MethodView):
+        #     for method in view.methods:
+        #         if method in rule.methods:
+        #             method_name = method.lower()
+        #             method = getattr(view.view_class, method_name)
+        #             operations[method_name] = yaml_utils.load_yaml_from_docstring(
+        #                 method.__doc__
+        #             )
         return rule.rule
