3b55b8f7e344512527bc62d74008a960c3c36408
Renato Teixeira <rena2damas@gmail.com>
2022-01-31 14:16:47 +0000

    refactor: add property summary based on function docstring #wip

--- src/apispec_plugins/helpers.py
+++ src/apispec_plugins/helpers.py
@@ -4,7 +4,17 @@ from apispec import yaml_utils
 
 
 def spec_from(specs):
+
+    # character sequence used by APISpec to separate
+    # yaml specs from the rest of the method docstring
+    yaml_sep = '---'
+
     def decorator(func):
+
+        docstring = func.__doc__
+        summary = docstring.split(yaml_sep)[0] if yaml_sep in docstring else None
+        if 'summary' not in specs and summary:
+            specs['summary'] = summary
 
         func.__doc__ = yaml_utils.dict_to_yaml(specs)
 
