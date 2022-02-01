cb2b580e7623fc80a0ecca3e6b5a0dc28b934f29
Renato Teixeira <rena2damas@gmail.com>
2022-02-01 11:30:45 +0000

    Add load_specs_from_docstring

--- src/apispec_plugins/helpers.py
+++ src/apispec_plugins/helpers.py
@@ -5,21 +5,14 @@ from apispec import yaml_utils
 
 def spec_from(specs):
 
-    # character sequence used by APISpec to separate
-    # yaml specs from the rest of the method docstring
-    yaml_sep = '---'
-
     def decorator(func):
 
-        docstring = func.__doc__
-        summary = docstring.split(yaml_sep)[0] if docstring else ''
-        summary = yaml_utils.trim_docstring(summary)  # sanitize
-        if 'summary' not in specs and summary:
-            specs['summary'] = summary
+        method_specs = load_specs_from_docstring(func.__doc__)
 
-        yaml_specs = yaml_utils.dict_to_yaml(specs)
-        yaml_specs = '\n'.join((summary, yaml_sep, yaml_specs))
-        func.__doc__ = yaml_specs
+        # merge specs prioritizing decorator specs
+        method_specs.update(specs)
+
+        func.__doc__ = yaml_utils.dict_to_yaml(method_specs)
 
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -27,3 +20,20 @@ def spec_from(specs):
         return wrapper
 
     return decorator
+
+
+def load_specs_from_docstring(docstring):
+
+    # character sequence used by APISpec to separate
+    # yaml specs from the rest of the method docstring
+    yaml_sep = '---'
+
+    if not docstring:
+        return {}
+
+    summary = docstring.split(yaml_sep)[0] if yaml_sep in docstring else ''
+    specs = yaml_utils.load_yaml_from_docstring(docstring)
+    if 'summary' not in specs and summary:
+        specs['summary'] = summary.strip()
+
+    return specs
