a6c5ec575ef4f2f96f65f45a52f03369672aaa85
Renato Teixeira <rena2damas@gmail.com>
2022-02-04 22:12:41 +0000

    test: add test_function_test

--- src/tests/webframeworks/test_flask.py
+++ src/tests/webframeworks/test_flask.py
@@ -1,29 +1,41 @@
-import unittest
-
 import pytest
 from apispec import APISpec
+from apispec_plugins.webframeworks.flask import FlaskPlugin
 from flask import Flask
 
-from src.apispec_plugins.webframeworks.flask import FlaskPlugin
+from .. import utils
 
 
-@pytest.fixture
+@pytest.fixture()
 def app():
     app = Flask(__name__)
     with app.test_request_context():
         yield app
 
 
-@pytest.fixture(scope='module', params=("2.0.0", "3.0.0"))
+@pytest.fixture(scope='module', params=('2.0.0', '3.0.0'))
 def spec(request):
     return APISpec(
-        title="Test Suite",
-        version="1.0.0",
+        title='Test Suite',
+        version='1.0.0',
         openapi_version=request.param,
         plugins=(FlaskPlugin(),),
     )
 
 
-class TestFlaskPlugin(unittest.TestCase):
+class TestFlaskPlugin:
 
-    pass
+    def test_function_view(self, app, spec):
+        @app.route('/foo')
+        def foo():
+            return 'bar'
+
+        operations = {'get': {
+            'parameters': [],
+            'responses': {'200': {}}
+        }}
+        spec.path(view=foo, operations=operations)
+        paths = utils.get_paths(spec)
+
+        assert 'get' in paths['/foo']
+        assert paths['/foo'] == operations
