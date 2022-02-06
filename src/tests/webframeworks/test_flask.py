6d11b41419a6af016b04d992cdc0bb5a25890ee5
Renato Teixeira <rena2damas@gmail.com>
2022-02-06 22:13:37 +0000

    Add test for testing a method view

--- src/tests/webframeworks/test_flask.py
+++ src/tests/webframeworks/test_flask.py
@@ -2,6 +2,7 @@ import pytest
 from apispec import APISpec
 from apispec_plugins.webframeworks.flask import FlaskPlugin
 from flask import Flask
+from flask.views import MethodView
 
 from .. import utils
 
@@ -13,7 +14,8 @@ def app():
         yield app
 
 
-@pytest.fixture(scope='module', params=('2.0.0', '3.0.0'))
+@pytest.fixture(scope='module', params=('2.0.0',))
+# @pytest.fixture(scope='module', params=('2.0.0', '3.0.0'))
 def spec(request):
     return APISpec(
         title='Test Suite',
@@ -26,16 +28,51 @@ def spec(request):
 class TestFlaskPlugin:
 
     def test_function_view(self, app, spec):
-        @app.route('/foo')
-        def foo():
-            return 'bar'
+        @app.route('/hi')
+        def hi():
+            return 'hi'
 
         operations = {'get': {
             'parameters': [],
             'responses': {'200': {}}
         }}
-        spec.path(view=foo, operations=operations)
+        spec.path(view=hi, operations=operations)
         paths = utils.get_paths(spec)
 
-        assert 'get' in paths['/foo']
-        assert paths['/foo'] == operations
+        assert 'get' in paths['/hi']
+        assert paths['/hi'] == operations
+
+    def test_method_view(self, app, spec):
+        class GreetingView(MethodView):
+            """The greeting view.
+            ---
+            x-extension: global metadata
+            """
+
+            def get(self):
+                """Get a greeting.
+                ---
+                description: request a new greeting
+                responses:
+                    200:
+                        description: the requested greeting
+                """
+                return 'hi'
+
+            def post(self):
+                return 204, {}
+
+        method_view = GreetingView.as_view('hi')
+        app.add_url_rule('/hi', view_func=method_view, methods=('GET', 'POST'))
+        spec.path(view=method_view)
+        paths = utils.get_paths(spec)
+
+        print(paths)
+
+        assert paths['/hi']['get'] == {
+            'summary': 'Get a greeting.',
+            'description': 'request a new greeting',
+            'responses': {'200': {'description': 'the requested greeting'}},
+        }
+        assert paths['/hi']['post'] == {}
+        assert paths['/hi']['x-extension'] == 'global metadata'
