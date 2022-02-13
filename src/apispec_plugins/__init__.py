5236afca6b30a82f9ff5ee3511b87f49aff58a25
Renato Teixeira <rena2damas@gmail.com>
2022-02-13 11:51:39 +0000

    Change to importlib metadata

--- src/apispec_plugins/__init__.py
+++ src/apispec_plugins/__init__.py
@@ -1,6 +1,10 @@
-import pkg_resources
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata  # python<=3.7
+
 from .utils import spec_from
 from .webframeworks.flask import FlaskPlugin
 
-__version__ = pkg_resources.get_distribution("apispec-plugins").version
+__version__ = importlib.metadata.version("apispec-plugins")
 __all__ = (FlaskPlugin, spec_from)
