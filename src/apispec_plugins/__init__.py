ed4993586ee391157d3e27503542fae89471c29b
Renato Teixeira <rena2damas@gmail.com>
2022-02-13 09:18:50 +0000

    refactor: set package __version__ #wip

--- src/apispec_plugins/__init__.py
+++ src/apispec_plugins/__init__.py
@@ -1,7 +1,6 @@
-__version_info__ = (0, 0, 1)
-__version__ = ".".join(str(c) for c in __version_info__)
-
+import importlib.metadata
 from .utils import spec_from
 from .webframeworks.flask import FlaskPlugin
 
+__version__ = importlib.metadata.version("apispec-plugins")
 __all__ = (FlaskPlugin, spec_from)
