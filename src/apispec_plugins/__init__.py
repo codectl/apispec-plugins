3e7fd56ea1084dcd22b4485c4b5b7d5691b805c7
Renato Teixeira <rena2damas@gmail.com>
2022-02-13 11:32:27 +0000

    Set package __version__

--- src/apispec_plugins/__init__.py
+++ src/apispec_plugins/__init__.py
@@ -1,6 +1,6 @@
-import importlib.metadata
+import pkg_resources
 from .utils import spec_from
 from .webframeworks.flask import FlaskPlugin
 
-__version__ = importlib.metadata.version("apispec-plugins")
+__version__ = pkg_resources.get_distribution("apispec-plugins").version
 __all__ = (FlaskPlugin, spec_from)
