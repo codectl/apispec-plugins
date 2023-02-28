90720f0c108d2bc4148f3e59479fe9fc6984041e
Renato Damas <me@renatodamas.com>
2023-02-28 16:46:02 +0000

    refactor: define __all___ module property

--- src/apispec_plugins/base/registry.py
+++ src/apispec_plugins/base/registry.py
@@ -1,26 +1,31 @@
-class RegistryError(NameError):
-    """Raised on every class registry error, for instance,
-    when a class lookup does not find the class.
-    """
+from typing import TypeVar
+
+from pydantic.main import ModelMetaclass
+
+__all__ = (
+    "RegistryBase",
+    "PydanticRegistry",
+    "RegistryError",
+)
+
+T = TypeVar("T")
 
 
 class RegistryBase(type):
 
-    _registry: dict[str, type] = {}
+    _registry: dict[str, T] = {}
 
     def __new__(mcs, name, bases, attrs):
-        # instantiate a new type corresponding to the type of class being defined
-        # this is currently RegisterBase but in child classes will be the child class
         cls = type.__new__(mcs, name, bases, attrs)
         mcs._registry[cls.__name__] = cls
         return cls
 
     @classmethod
     def get_registry(mcs):
-        return dict(mcs._registry)
+        return mcs._registry
 
     @classmethod
-    def get_cls(mcs, classname: str) -> type:
+    def get_cls(mcs, classname: str) -> T:
         try:
             return mcs._registry[classname]
         except KeyError as error:
@@ -28,3 +33,13 @@ class RegistryBase(type):
                 f"Class with name {classname!r} was not found. You may need "
                 "to import the class."
             ) from error
+
+
+class PydanticRegistry(ModelMetaclass, RegistryBase):
+    """Harness registry metaclass for pydantic"""
+
+
+class RegistryError(NameError):
+    """Raised on every class registry error, for instance,
+    when a class lookup does not find the class.
+    """
